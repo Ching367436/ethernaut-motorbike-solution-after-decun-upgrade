69378fe0f0af9f33d9454b9ca352d61ad60fe39f
Ching367436 <44335786+Ching367436@users.noreply.github.com>
2024-06-12 18:36:58 +0800

    git: remove unnecessary files

--- contracts/Selfdestruct.sol
+++
@@ -1,8 +0,0 @@
-// https://ethereum.stackexchange.com/a/145050
-pragma solidity ^0.8;
-
-contract Selfdestruct {
-    fallback() external payable {
-        selfdestruct(payable(tx.origin));
-    }
-}
